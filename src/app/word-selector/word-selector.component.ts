f0a3e1fbb3a938cbce9cfc54991541247267e6b1
TomCorson <corsontom@yahoo.com>
2021-01-17 16:28:36 -0500

    word-selector

---
+++ src/app/word-selector/word-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-selector',
+  templateUrl: './word-selector.component.html',
+  styleUrls: ['./word-selector.component.css']
+})
+export class WordSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
