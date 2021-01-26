675fac773c51c9a3f446f636816a6cb9716a8b96
TomCorson <corsontom@yahoo.com>
2021-01-26 09:57:51 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WordSelectorComponent } from './word-selector/word-selector.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'word-selector', component: WordSelectorComponent },
+  { path: 'home-page', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [WordSelectorComponent, HomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { WordSelectorComponent } from './word-selector/word-selector.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    WordSelectorComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
