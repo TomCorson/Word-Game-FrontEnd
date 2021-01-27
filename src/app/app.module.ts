18586f22e611a6c1ac901962cb680d47adf6f7f3
TomCorson <corsontom@yahoo.com>
2021-01-27 17:48:59 -0500

    create config-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ConfigServiceComponent } from './config-service/config-service.component';
+
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
   declarations: [
     AppComponent,
     NavbarComponent,
-    routingComponents
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    routingComponents,
+    ConfigServiceComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config-service/config-service.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfigServiceComponent } from './config-service.component';
+
+describe('ConfigServiceComponent', () => {
+  let component: ConfigServiceComponent;
+  let fixture: ComponentFixture<ConfigServiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConfigServiceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfigServiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
