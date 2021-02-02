fcf90ea1a0014dd8963c31e90a1414cc9b5146ca
TomCorson <corsontom@yahoo.com>
2021-02-01 20:04:32 -0500

    create MadLib object

--- src/app/word-selector/word-selector.component.ts
+++ src/app/word-selector/word-selector.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {ConfigService} from '../config.service';
+import {MadLib} from './mad-lib';
 
 @Component({
   selector: 'app-word-selector',
@@ -17,7 +18,18 @@ export class WordSelectorComponent implements OnInit {
   }
   getMadLibList(): any[] {
     this.configService.getMadLibs()
-      .subscribe( response => {})
+      .subscribe( response => {
+        for(const MadLibResponse of response){
+          const MadLibObject = new MadLib(
+            MadLibResponse.id,
+            MadLibResponse.name,
+            MadLibResponse.story,
+            MadLibResponse.partsOfSpeech
+          );
+          this.MadLibList.push(MadLibObject);
+        }
+      });
+    return this.MadLibList;
   }
 
 }
