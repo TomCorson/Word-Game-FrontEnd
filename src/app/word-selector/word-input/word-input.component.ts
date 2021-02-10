2bd0ff43118e3520519dca7072a5a394ec42047d
TomCorson <corsontom@yahoo.com>
2021-02-09 21:40:46 -0500

    working on input

--- src/app/word-selector/word-input/word-input.component.ts
+++ src/app/word-selector/word-input/word-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { MadLib } from './mad-lib';
+
 
 @Component({
   selector: 'app-word-input',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./word-input.component.css']
 })
 export class WordInputComponent implements OnInit {
-
+  @Input() madlib: MadLib | undefined;
   constructor() { }
 
   ngOnInit(): void {
